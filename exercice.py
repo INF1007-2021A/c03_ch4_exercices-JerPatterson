5971abe3e86b084b41d059dcb2cd97f0ef5a1046
JerPatterson <you@example.com>
2021-09-14 10:11:11 -0400

    with all exercises completed

--- exercice.py
+++ exercice.py
@@ -8,20 +8,39 @@ def is_even_len(string: str) -> bool:
 
 
 def remove_third_char(string: str) -> str:
-    
-    return ""
+    letters_except_third = string [:2] + string [3:]
+    new_string = letters_except_third
+    return new_string
 
 
 def replace_char(string: str, old_char: str, new_char: str) -> str:
-    return ""
+    new_string = ""
+    for letter in string: 
+        if ord(letter) == ord(old_char): 
+            letter = new_char
+        else: letter = letter 
+        new_string += letter
+        
+    return new_string
 
 
 def get_nb_char(string: str, char: str) -> int:
-    return 0
+    print(string)
+    number_of_char = 0
+    for letter in string:
+        if ord(letter) == ord(char):
+            number_of_char += 1
+        else: number_of_char += 0
+    return number_of_char
 
 
 def get_nb_words(sentence: str) -> int:
-    return 0
+    number_of_word = 1
+    for letter in sentence:
+        if ord(letter) == 32:
+            number_of_word += 1
+        else: number_of_word += 0
+    return number_of_word
 
 
 def main() -> None:
